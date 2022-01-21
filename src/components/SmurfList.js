cbc6d4ae8013642cbfcf557a3ed38979e5f43f5a
Winifred Lu <winnie.lu@comcast.net>
2022-01-21 11:44:44 -0500

    smurflist refactored, testing required

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -5,7 +5,6 @@ import SmurfContext from '../contexts/SmurfContext';
 
  const SmurfList = ()=> {
     const {loading, smurfs} = useContext(SmurfContext);
-    const isLoading = false;
     const testSmurf = {
         id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
         name:'Poppa Smurf',
@@ -14,12 +13,16 @@ import SmurfContext from '../contexts/SmurfContext';
         description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
     }
 
-    if (isLoading) {
+    if (loading) {
         return <h1>Loading...</h1>;
     }
 
     return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
+        {
+            smurfs.map(smurf => {
+                return <Smurf />
+            })
+        }
     </div>);
 }
 
